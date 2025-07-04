a9c24d6e9eee003eb9115fbc9a707a75b885d8c3
Md Khalid <mdkhalidkhas27@gmail.com>
2025-07-04 22:16:35 +0530

    Remove the unwanted file

--- generatecontent.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-prompt=input("Enter your prompt:")
-client = genai.Client(api_key="AIzaSyDn8qVqu6Sqhv-_agnP2A18JTPq-4Fp4AM")
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=prompt
-)
-print(response.text)
